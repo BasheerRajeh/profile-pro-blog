94a8e049f078e8e1d670b8afac929759b45e3d8d
Basheer Rajeh <muhammadbasheerrajeh@hotmail.com>
2023-10-31 17:51:08 +0300

    chore: connect prisma with next-auth

--- lib/next-auth.ts
+++ lib/next-auth.ts
@@ -1,11 +1,23 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import { NextAuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
+import db from './db'
+
 export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(db),
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }),
     ],
+    callbacks: {
+        session: ({ session, user }) => {
+            session.user.id = user.id
+            return session
+        },
+    },
+    debug: process.env.NODE_ENV === 'development',
+    secret: process.env.NEXTAUTH_SECRET,
 }
